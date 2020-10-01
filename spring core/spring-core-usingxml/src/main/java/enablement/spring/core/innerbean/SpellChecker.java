3b8e7c0687208e0e29cd4486555f32b696a60a11
Rosary7 <amirtrichy@gmail.com>
2020-10-01 13:17:29 +0530

    SpellChecker
    SpellChecker

---
+++ spring core/spring-core-usingxml/src/main/java/enablement/spring/core/innerbean/SpellChecker.java
@@ -0,0 +1,11 @@
+package enablement.spring.core.innerbean;
+
+public class SpellChecker {
+	public SpellChecker() {
+		System.out.println("Inside SpellChecker constructor.");
+	}
+
+	public void checkSpelling() {
+		//System.out.println("Inside checkSpelling.");
+	}
+}
