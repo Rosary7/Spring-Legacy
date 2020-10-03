c66cf2afdc9854fadc262cfd0f6e4d7bac5b5822
Rosary7 <amirtrichy@gmail.com>
2020-10-03 15:53:50 +0530

    model
    model

---
+++ SpringMVC/springmvc-bsaics/src/main/learn/springmvc/springmvc_bsaics/model/Shop.java
@@ -0,0 +1,22 @@
+package learn.springmvc.springmvc_bsaics.model;
+
+public class Shop {
+	String shopName;
+	String staffName[];
+	
+	
+	public String getShopName() {
+		return shopName;
+	}
+	public void setShopName(String shopName) {
+		this.shopName = shopName;
+	}
+	public String[] getStaffName() {
+		return staffName;
+	}
+	public void setStaffName(String[] staffName) {
+		this.staffName = staffName;
+	}
+	
+	
+}
